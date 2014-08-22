4cc38ca54b012a1b0b012c6a2137a80c7eb49cd4
Smbat Yeranyan <smbat.yeranyan@gmail.com>
2014-08-22 17:06:49 +0200

    Update protected.js

--- www/js/app/controllers/protected.js
+++ www/js/app/controllers/protected.js
@@ -8,15 +8,17 @@ app.controller('protectedController', function($scope,$location,$rootScope,setti
 
   });
   
+  //Include your Relayr Keys
   var appId= "456def81-1c82-491f-8738-2d14d12c8e18";
   var deviceId= "8b0621a7-0a5f-407d-9172-3fa8f7bd6b89";
   var token ="uwsyX8zEcPLXHeInhuppyvcn_VUrtTwK";
 
 
-//POST to the API with the appId and deviceId in the url to get the credentials for Pubnub
+  //POST to the API with the appId and deviceId in the url to get the credentials for Pubnub
 
   $(document).ready(function(){
-
+    
+    //Use Ajax to get PubNub credentials from Relayr
     $.ajax({
         url: "https://api.relayr.io/apps/"+appId+ "/devices/"+ deviceId,
         type: 'post',
@@ -27,6 +29,8 @@ app.controller('protectedController', function($scope,$location,$rootScope,setti
         complete: function (subscription) {
           console.log(angular.fromJson(subscription.responseText))
           subscription = angular.fromJson(subscription.responseText);
+          
+          //connect to pubnub using the credentials from relayr
           var pubnub = PUBNUB.init({
             subscribe_key : subscription.subscribeKey,
             ssl: true,
@@ -40,7 +44,9 @@ app.controller('protectedController', function($scope,$location,$rootScope,setti
             channel : subscription.channel,
 
             message : function(data){
+              //Listen for the raw data
               data = angular.fromJson(data)
+              
               if (data.prox < 2000){
                     
                 $location.path("/alarm");
